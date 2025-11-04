12f039c925141bf11d2a8ecd6eeccd2fe40cf00f
Pham Quoc Khanh <khanhphamquocnb@gmail.com>
2025-11-04 21:49:11 +0700

    Sidebar admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Dashboard from './Pages/Dashboard';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      exact:true,
+      element: (
+        <>
+          <section className='main'>
+            <Header/>
+            <div className='contentMain flex'>
+              <div className='sidebarWrapper w-[25%]'>
+                <Sidebar/>
+              </div>
+            </div>
+          </section>
+        </>
+      ),
+      
+    },
+  ]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-gray-500">Admin</h1>
+      <RouterProvider router={router}/>
     </>
-  )
+  );
 }
 
 export default App
